e882d4c8b12f056dfa50a69e28cacaff001f755e
Thiago Brito Rocha <ctl_thirocha@uolinc.com>
2023-05-16 08:57:38 -0300

    Adiciondo groovy..

--- deploy/Deploy.groovy
+++ deploy/Deploy.groovy
@@ -3,17 +3,19 @@ pipeline {
     agent any
     stages {
         stage("One") {
-            script {
-                def gradleProperties = readProperties file: 'gradle.properties'
-                def currentVersion = gradleProperties['version']
-                def versionParts = currentVersion.split('\\.')
-                def majorVersion = Integer.parseInt(versionParts[0])
-                def minorVersion = Integer.parseInt(versionParts[1])
-                minorVersion++
-                def newVersion = "${majorVersion}.${minorVersion}"
-                echo "${newVersion}"
-                gradleProperties['version'] = newVersion
-                writeProperties file: 'gradle.properties', properties: gradleProperties
+            steps {
+                script {
+                    def gradleProperties = readProperties file: 'gradle.properties'
+                    def currentVersion = gradleProperties['version']
+                    def versionParts = currentVersion.split('\\.')
+                    def majorVersion = Integer.parseInt(versionParts[0])
+                    def minorVersion = Integer.parseInt(versionParts[1])
+                    minorVersion++
+                    def newVersion = "${majorVersion}.${minorVersion}"
+                    echo "${newVersion}"
+                    gradleProperties['version'] = newVersion
+                    writeProperties file: 'gradle.properties', properties: gradleProperties
+                }
             }
         }
     }
