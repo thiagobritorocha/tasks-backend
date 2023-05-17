c6c7f122f186084bd277df20ef1554825cf7184b
Thiago Brito Rocha <ctl_thirocha@uolinc.com>
2023-05-17 16:31:28 -0300

    Adicionando groovy..

--- deploy/Deploy.groovy
+++ deploy/Deploy.groovy
@@ -4,17 +4,19 @@ pipeline {
     stages {
         stage("build") {
             steps {
-                withGroovy(tool:'4.0.8') {
-                    def gradleProperties = readProperties file: 'gradle.properties'
-                    def currentVersion = gradleProperties['version']
-                    def versionParts = currentVersion.split('\\.')
-                    def majorVersion = Integer.parseInt(versionParts[0])
-                    def minorVersion = Integer.parseInt(versionParts[1])
-                    minorVersion++
-                    def newVersion = "${majorVersion}.${minorVersion}"
-                    echo "${newVersion}"
-                    gradleProperties['version'] = newVersion
-                    writeProperties file: 'gradle.properties', properties: gradleProperties
+                step {
+                    withGroovy(tool: '4.0.8') {
+                        def gradleProperties = readProperties file: 'gradle.properties'
+                        def currentVersion = gradleProperties['version']
+                        def versionParts = currentVersion.split('\\.')
+                        def majorVersion = Integer.parseInt(versionParts[0])
+                        def minorVersion = Integer.parseInt(versionParts[1])
+                        minorVersion++
+                        def newVersion = "${majorVersion}.${minorVersion}"
+                        echo "${newVersion}"
+                        gradleProperties['version'] = newVersion
+                        writeProperties file: 'gradle.properties', properties: gradleProperties
+                    }
                 }
             }
         }
