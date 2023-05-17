d18039d2fe8506e9e2e5d3544a53da5f23dd969a
Thiago Brito Rocha <ctl_thirocha@uolinc.com>
2023-05-17 16:36:15 -0300

    Adicionando groovy..

--- deploy/Deploy.groovy
+++ deploy/Deploy.groovy
@@ -1,34 +1,43 @@
-#!groovy
-pipeline {
-    agent any
-    stages {
-        stage("build") {
-            steps {
-                script {
-                    withGroovy(tool: '4.0.8') {
-                        def gradleProperties = readProperties file: 'gradle.properties'
-                        def currentVersion = gradleProperties['version']
-                        def versionParts = currentVersion.split('\\.')
-                        def majorVersion = Integer.parseInt(versionParts[0])
-                        def minorVersion = Integer.parseInt(versionParts[1])
-                        minorVersion++
-                        def newVersion = "${majorVersion}.${minorVersion}"
-                        echo "${newVersion}"
-                        gradleProperties['version'] = newVersion
-                        writeProperties file: 'gradle.properties', properties: gradleProperties
-                    }
-                }
-            }
-        }
-        stage("test") {
-            steps {
-                echo 'test'
-            }
-        }
-        stage("deploy") {
-            steps {
-                echo 'deploy'
+#!/usr/bin/env groovy
+
+node {
+    stage('Build') {
+        steps {
+            script {
+                def propertiesFile = readFile 'gradle.properties'
+                def versionProperty = findProperty(propertiesFile, 'version')
+
+                // Incrementa a versão em 1
+                def incrementedVersion = incrementVersion(versionProperty)
+
+                // Atualiza a propriedade no arquivo gradle.properties
+                propertiesFile = updateProperty(propertiesFile, 'version', incrementedVersion)
+
+                // Salva o arquivo atualizado
+                writeFile file: 'gradle.properties', text: propertiesFile
             }
         }
     }
+}
+
+def findProperty(fileContent, propertyName) {
+    def properties = new Properties()
+    properties.load(new ByteArrayInputStream(fileContent.bytes))
+    properties.getProperty(propertyName)
+}
+
+def incrementVersion(version) {
+    def versionParts = version.split('\\.')
+    def lastPart = versionParts[-1].toInteger()
+    versionParts[-1] = (lastPart + 1).toString()
+    versionParts.join('.')
+}
+
+def updateProperty(fileContent, propertyName, newValue) {
+    def properties = new Properties()
+    properties.load(new ByteArrayInputStream(fileContent.bytes))
+    properties.setProperty(propertyName, newValue)
+    def outputStream = new ByteArrayOutputStream()
+    properties.store(outputStream, null)
+    outputStream.toString()
 }
